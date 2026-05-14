bf073850aa2cee584dd90a391a63fe23bd2c6f80
EmillyF0 <emillyfrancine2007@gmail.com>
2026-05-14 00:04:39 +0000

    exec3.c

---
+++ Lista 6/exec3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int numeros[10];
+    int i, maior;
+    
+    for (i=0;i<10;i++) {
+        printf ("Digite o número %d°:\n", i);
+        scanf ("%d", &numeros[i]);
+    }
+    maior = numeros[0];
+    for (i=0;i<10;i++) {
+        if (numeros[i] > maior) {
+            maior = numeros[i];
+        }
+    }
+    
+    printf ("Maior valor = %d\n", maior);
+    
+    return 0;
+}
